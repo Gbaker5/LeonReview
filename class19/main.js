5b356be946bcf2277a1cad87931bab9ec33382c1
Gjarred Baker <gjarred23@gmail.com>
2024-01-09 08:05:10 -0600

    min

--- class19/main.js
+++ class19/main.js
@@ -307,3 +307,13 @@ function squareIt(){
   }
   
   squareIt()
+
+  function min(x,y){
+    if(x < y){
+        return x
+    }else return y
+  }
+
+console.log(min(4.5, 5)); // Must show 4.5
+console.log(min(19, 9));  // Must show 9
+console.log(min(1, 1));   // Must show 1
