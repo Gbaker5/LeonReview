42c712f1ca553622ee75a900253f6f4d8c9c77c1
Gjarred Baker <gjarred23@gmail.com>
2024-01-16 20:27:32 -0600

    ch 2

--- class19/main.js
+++ class19/main.js
@@ -14,3 +14,38 @@ console.log(6 * 3)
 console.log( 6 / 3)
 console.log()
 
+//chapter 2
+
+//const fullname = prompt("What is your first and last name");
+//alert(`Hello ${fullname}`)
+
+let a = 2;
+a -= 1;
+a++;
+let b = 8;
+b += 2;
+const c = a + b * b;
+const d = a * b + b;
+const e = a * (b + b);
+const f = a * b / a;
+const g = b / a * a;
+console.log(a, b, c, d, e, f, g);
+ //
+//const rawNum = prompt("What is the raw num?");
+//const vat = Number(rawNum);
+//console.log(vat / 1.20)
+
+//
+//const celsius = prompt("what is the celsius degree?");
+//const celsiusNum = Number(celsius);
+//console.log(`Ferenheit from Celsius degree is ${celsius * (9/5) + 32}`)
+
+//
+let number1 = 5;
+let number2 = 3;
+
+number1 = number1 - 2;
+number2 = number2 + 2;
+
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
