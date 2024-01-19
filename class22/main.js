934b74289fca29d8438db52c6550ea8d52783e26
Gjarred Baker <gjarred23@gmail.com>
2024-01-19 08:05:10 -0600

    aurora

--- class22/main.js
+++ class22/main.js
@@ -37,3 +37,28 @@ function sayStop(){
 }
 
 sayStop()
+
+const aurora = {
+    health:150,
+    strength:25,
+    xp:0,
+
+    describe(){
+        return `Aurora has ${this.health} health points, ${this.strength} as strength and ${this.xp} xp points`
+    }
+};
+
+// TODO: create the character object here
+
+// Aurora is harmed by an arrow
+aurora.health -= 20;
+
+// Aurora equips a strength necklace
+aurora.strength += 10;
+
+// Aurora learn a new skill
+aurora.xp += 15;
+
+
+
+console.log(aurora.describe());
