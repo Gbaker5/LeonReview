60d26fbb6cc24663b6f4c07b762d176bc5d0e752
Gjarred Baker <gjarred23@gmail.com>
2024-01-24 08:05:10 -0600

    fetch once

--- class28/cocktail/main.js
+++ class28/cocktail/main.js
@@ -1,19 +1,23 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+let isFetchedCalled = false;
+
 document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink(){
 
+if(isFetchedCalled){
+    console.log("Cannot Fetch Again!")
+}else 
 
-
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=y')
     .then(res => res.json()) // parse response as JSON
     .then(data => {
         //console.log(data)
-      console.log(data.drinks[0])
-      //data.drinks.length
-      document.querySelector('h2').innerText = data.drinks[0].strDrink
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb
-      document.querySelector('h3').innerText = data.drinks[0].strInstructions
+      //console.log(data.drinks[0])
+      //console.log(data.drinks.length)
+      //document.querySelector('h2').innerText = data.drinks[0].strDrink
+      //document.querySelector('img').src = data.drinks[0].strDrinkThumb
+      //document.querySelector('h3').innerText = data.drinks[0].strInstructions
 
       for(i=0;i<data.drinks.length;i++){
 
@@ -31,13 +35,13 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
 
       //image
       const newImg = document.createElement('img'); //create image element
-      newImg.src = data.drinks[0].strDrinkThumb; //change source
-      document.getElementById('slide1').appendChild(newImg) //append to li (inside)
+      newImg.src = data.drinks[i].strDrinkThumb; //change source
+      document.getElementById('slide' + [i]).appendChild(newImg) //append to li (inside)
 
       //ingredients
             //ingredients list
             let ingredientList = [];
-            const keys = Object.keys(data.drinks[0]) //keys of drink object
+            const keys = Object.keys(data.drinks[i]) //keys of drink object
             //console.log(keys)
             const ingredientKeys = keys.filter( property => property.includes("strIngredient")) //ingredients keys
             //console.log(ingredientKeys) 
@@ -57,19 +61,21 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
 
       const newIngredients = document.createElement('p'); //create p element
       newIngredients.innerText = `Ingredients: ${ingredientsString}`;// insert text
-      document.getElementById('slide1').appendChild(newIngredients)// add to Li
+      document.getElementById('slide' + [i]).appendChild(newIngredients)// add to Li
       
 
 
         //instructions
      const newInstruct = document.createElement('p'); //create p element
-     newInstruct.innerText = 'Instructions: ' + data.drinks[0].strInstructions;// insert text
-     document.getElementById('slide1').appendChild(newInstruct)// add to Li
+     newInstruct.innerText = 'Instructions: ' + data.drinks[i].strInstructions;// insert text
+     document.getElementById('slide' + [i]).appendChild(newInstruct)// add to Li
 
 
 
       //const allDrinks = data.drinks
       //console.log(allDrinks)
+
+      isFetchedCalled = true;
     }
 
     })
