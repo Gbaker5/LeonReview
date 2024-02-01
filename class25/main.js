7664460fce022b8797c34728bb2592417ddc244e
Gjarred Baker <gjarred23@gmail.com>
2024-02-01 13:04:39 -0600

    stopWatch

--- class25/main.js
+++ class25/main.js
@@ -23,7 +23,7 @@ employee.getWage();
 
 console.log("hello world")
 
-//object  literal
+//OBJECT LITERAL
 //const circle = {
     //radius: 1, //property
     //location: { //property
@@ -39,7 +39,7 @@ console.log("hello world")
 
 //if it has more than one method we say that it has behaviors
 
-//Factory reconstructor function 
+//FACTORY RECONSTRUCTOR FUNCTION
 function createCircle(radius){
    return {
         radius, //property
@@ -53,28 +53,41 @@ function createCircle(radius){
 
 //circle.draw();
 
-//constructor Function
+//CONSTRUCTOR FUNCTION
 
-function Circle(radius){
-    console.log('this', this)
-    this.radius= radius;
-    this.draw = function(){
-        console.log(draw);  
-    }
-}
-
-
-const circle = new Circle(10);
-
-circle.location = { x: 1 }; //adding a property
-
-const propertyName = 'location';
+//function Circle(radius){
+    //console.log('this', this)
+    //this.radius= radius;
+    //this.draw = function(){
+        //console.log(draw);  
+    //}
+//}
 
 
-circle['location'] = { x: 1}; //same thing just bracket notation
+//const circle = new Circle(10);
 
-delete circle.location;
-delete circle['location']
+//FOR IN LOOP - TO ITERATE THROUGH OBJECT
+//for(let key in circle){
+   // if(typeof circle[key] !== 'function')// only properties not methods - this excludes functions
+   // console.log(key, circle[key]) //key = peoperties , circle[key] = values of keys
+//}
+
+//const keys = Object.keys(circle)
+//console.log(keys)
+
+//if('radius' in circle)
+//console.log("Circle has radius")
+
+
+//ADDING AND DELETING PROPERTIES
+//circle.location = { x: 1 }; //adding a property
+
+//const propertyName = 'location';
+
+//circle['location'] = { x: 1}; //adding a property using bracket notation
+
+//delete circle.location;
+//delete circle['location']
 
 
 
@@ -117,7 +130,7 @@ function increase(number){
 }
 
 increase(number);
-console.log(number)
+//console.log(number)
 
 //reference
 
@@ -129,4 +142,82 @@ function increase(obj){
 
 increase(obj);
 increase(obj)
-console.log(obj)
+//console.log(obj)
+
+
+function Circle(radius){
+    
+    this.radius= radius;
+
+    let defaultLocation = { x:0, y:0}; //cannot be accsed outside of this function because it is stored in variable instead of using this.defaultLocation
+
+    let computeOptimumLocation = function(factor){
+        //..
+    }
+
+    this.getDefaultLocation = function(){
+        return defaultLocation;
+    }
+
+    this.draw = function(){
+        computeOptimumLocation(0.1);
+        //defaultLocation
+    }
+
+    //Object.defineProperty(this, 'defaultLocation', { //use objectdefine property to define getters and setters
+        //get: function (){
+            //return defaultLocation;
+        //},
+        //set: function(value){
+            //if(!value.x || !value.y)
+            //throw new Error('invalid location')
+
+            //defaultLocation = value;
+        //}
+    //})
+}
+
+
+const circle = new Circle(10);
+circle.defaultLocation = 1
+
+
+function StopWatch(){
+
+    let startTime, endTime, running, duration = 0; 
+
+    this.start = function(){
+        if (running)
+        throw new Error('Stopwatch is already running')
+
+        running = true;
+        
+        startTime = new Date();
+    };
+
+    this.stop = function(){
+        if (!running)
+        throw newError('Stopwatch has not started')
+
+        running = false;
+        
+        endTime = new Date();
+        
+        const seconds = (endTime.getTime() - startTime.getTime()) /1000;
+        duration += seconds
+    };
+
+    this.reset = function(){
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+
+    };
+
+    Object.defineProperty(this, 'duration',{
+        get: function( ) {return duration}
+    });
+}
+
+StopWatch.start()
