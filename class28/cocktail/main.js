6a4fbd26645030ec0c0c1b6cd1b1a154a87df8b3
Gjarred Baker <gjarred23@gmail.com>
2024-02-02 22:10:15 -0600

    new li and image

--- class28/cocktail/main.js
+++ class28/cocktail/main.js
@@ -14,6 +14,22 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
       document.querySelector('h2').innerText = data.drinks[0].strDrink
       document.querySelector('img').src = data.drinks[0].strDrinkThumb
       document.querySelector('h3').innerText = data.drinks[0].strInstructions
+
+      const newLi = document.createElement('li'); //create li element
+      newLi.classList.add("slide"); //add class
+      newLi.id = "slide1"; //add class
+
+      document.getElementById('data-slides').appendChild(newLi) //append li to ul
+
+      const newImg = document.createElement('img'); //create image element
+      newImg.src = data.drinks[0].strDrinkThumb; //change source
+      
+
+      document.getElementById('slide1').appendChild(newImg) //append to li (inside)
+
+      //const allDrinks = data.drinks
+      //console.log(allDrinks)
+
     })
     .catch(err => {
         console.log(`error ${err}`)
