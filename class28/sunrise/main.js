d6e0b89055c0214484947b6ce2a08f3fdd9973c6
Gjarred Baker <gjarred23@gmail.com>
2024-02-03 22:06:19 -0600

    html css

---
+++ class28/sunrise/main.js
@@ -0,0 +1,23 @@
+
+
+//document.querySelector('').addEventListener('click', getSun)
+
+
+function getSun(){
+
+}
+
+fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400')
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+  console.log(data)  
+    let sunrise = data.results.sunrise;
+    let sunset = data.results.sunset;
+    console.log(sunrise)
+    console.log(sunset)
+
+
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
