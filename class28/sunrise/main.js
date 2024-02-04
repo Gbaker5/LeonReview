ddc7adb615188426ffc3ed19991eb113d1a9a9e5
Gjarred Baker <gjarred23@gmail.com>
2024-02-04 14:57:53 -0600

    more js

--- class28/sunrise/main.js
+++ class28/sunrise/main.js
@@ -1,11 +1,25 @@
 
 
 document.querySelector('#getTime').addEventListener('click', getSun)
-
+let latitudeInputArr = [];
+let longitudeInputArr = [];
 
 function getSun(){
+ //
+let latitudes = document.querySelector('#latitude').value
+let longitudes = document.querySelector('#longitude').value
 
+latitudeInputArr.push(latitudes)
+longitudeInputArr.push(longitudes)
 
+console.log(latitudeInputArr)
+console.log(longitudeInputArr)
+
+let lastLatEntry = latitudeInputArr[latitudeInputArr.length - 1] //last input of 
+let lastLongEntry = longitudeInputArr[longitudeInputArr.length - 1]
+
+console.log(lastLatEntry)
+console.log(lastLongEntry)
 
 fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400')
 .then(res => res.json()) // parse response as JSON
