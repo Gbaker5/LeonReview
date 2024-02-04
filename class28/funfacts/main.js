9a4d9b724a41f0d096211a87f0129f2e06d04edd
Gjarred Baker <gjarred23@gmail.com>
2024-02-04 08:04:20 -0600

    html and some js

---
+++ class28/funfacts/main.js
@@ -0,0 +1,11 @@
+
+document.querySelector('button').addEventListener('click', getNewKeanu)
+
+function getNewKeanu(){
+    let width = document.querySelector('#width').value 
+    console.log(width)
+    let height = document.querySelector('#height').value 
+    console.log(height)
+}
+
+    
