d4c212dd3f2d45885ca0a6aa0517ce8b80c2e4f4
Gjarred Baker <gjarred23@gmail.com>
2024-02-06 08:05:10 -0600

    rick and morty

---
+++ class28/new/main.js
@@ -0,0 +1,34 @@
+
+
+
+
+let categoryInput= document.querySelector('#category').value
+let pageInput= document.querySelector('#page').value
+
+
+fetch('https://rickandmortyapi.com/api/character')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // Do something with the data
+
+    
+
+
+
+
+
+
+
+
+
+
+
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
