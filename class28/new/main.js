f62db5620f56711f7d8ab2aa72fb99fede111177
Gjarred Baker <gjarred23@gmail.com>
2024-02-06 08:05:10 -0600

    start js fetch

--- class28/new/main.js
+++ class28/new/main.js
@@ -5,9 +5,81 @@
 let categoryInput= document.querySelector('#category').value
 let pageInput= document.querySelector('#page').value
 
+    let nameArray =[];
+    let statusArray = [];
+    let genderArray = [];
+    let speciesArray = [];
+    let typeArray = [];
 //if categoryinput == "vfsv" {this fetch} else if(categoryInput == "dfvsf"){do this fetch}
+for(i=1;i<42;i++){
+fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
 
-fetch('https://rickandmortyapi.com/api/character')
+   
+    //let nameArray2 = []
+    
+    
+    for(k=0;k<data.results.length;k++){
+    //console.log(data); // Do something with the data
+    //console.log(data.results)
+    nameArray.push(data.results[k].name)
+    statusArray.push(data.results[k].status)
+    genderArray.push(data.results[k].gender)
+    speciesArray.push(data.results[k].species)
+    typeArray.push(data.results[k].type)
+    
+    }
+  
+    //console.log(nameArray)
+    //let allNames = nameArray.join(" ").split(" ")
+    //console.log(allNames)
+    //let uniqueNameArray = allNames.filter((item, index) => allNames.indexOf(item) == index)
+    //console.log(uniqueNameArray)
+    //let nameOrdered = uniqueNameArray.sort()
+    //console.log(nameOrdered)
+    
+    
+    console.log(statusArray)
+    
+    let uniqueStatusArray = statusArray.filter((item, index) => statusArray.indexOf(item) == index)
+    console.log(uniqueStatusArray)
+//
+    //console.log(genderArray)
+    //let uniqueGenderArray = nameArray.filter((item, index) => nameArray.indexOf(item) == index)
+    //console.log(uniqueGenderArray)
+//
+    //console.log(speciesArray)
+    //let uniqueSpecieseArray = nameArray.filter((item, index) => nameArray.indexOf(item) == index)
+    //console.log(uniqueSpecieseArray)
+//
+    //console.log(typeArray)
+    //let uniqueTypeArray = nameArray.filter((item, index) => nameArray.indexOf(item) == index)
+    //console.log(uniqueTypeArray)
+    
+
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+
+}
+
+
+
+
+
+  /////////////////////////
+
+document.querySelector('#allButton').addEventListener('click', getAll)
+
+function getAll(){
+    fetch('https://rickandmortyapi.com/api/character?name=sanchez')
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -19,17 +91,55 @@ fetch('https://rickandmortyapi.com/api/character')
 
     
 
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
 
 
+///////////////////
+document.querySelector('#allButton').addEventListener('click', getSpecificCharacters)
 
+function getSpecificCharacters(){
+    fetch('https://rickandmortyapi.com/api/character?name=sanchez')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // Do something with the data
 
-
-
-
-
-
+    
 
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
   });
+}
+
+
+
+////////////////////////
+document.querySelector('#allButton').addEventListener('click', getFilterCharacters)
+
+function getFilterCharacters(){
+    fetch('https://rickandmortyapi.com/api/character?name=sanchez')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // Do something with the data
+
+    
+
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
