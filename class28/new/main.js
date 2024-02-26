5c75746a6357d9bd79e034e5dc0b6a3d5f24dd53
Gjarred Baker <gjarred23@gmail.com>
2024-02-25 21:17:40 -0600

    add css js

--- class28/new/main.js
+++ class28/new/main.js
@@ -1635,9 +1635,114 @@ function getFilterCharacters(){
 
     
     
+    document.querySelector('#allResults').innerHTML = ""
+      document.querySelector('#footer').innerHTML = ""
+  
+      document.querySelector('.result-ct').classList.add("galaxyBack")
 
+      let nextPage = data.info.next 
+      for(i=0;i<data.results.length;i++){
+  
+  
+     
+     let theId = data.results[i].id //id - 1
+     let image = data.results[i].image//image 
+     let name = data.results[i].name //name - "Rick Sanchez"
+     let location = data.results[i].location.name //location - "Rick Citadel"
+     let gender = data.results[i].gender//gender = "Male"
+     let status = data.results[i].status//status = "Alive"
+     let species = data.results[i].species//species = "human"
+     let type = data.results[i].type//type = ""
+     let origin = data.results[i].origin.name//origin - "Earth"
+  
+  
+      //id
+      //name
+      //gender
+      //species
+      //type
+      //origin
+      //location
+      //status
+  
+      let newDiv = document.createElement('div') //create div
+      newDiv.classList.add('resultbox') //add resultbox class
+      newDiv.classList.add('resultbox' + theId) //add resultbox + id for js reference appending
+      document.querySelector('#allResults').appendChild(newDiv) //add div to ul
+  
+      
+  
+      let imageLi = document.createElement('li'); //create li for image
+      imageLi.classList.add('resultImage'); //add "resultImage" class
+      imageLi.classList.add('resultImage' + theId) //add '.resultImage1'
+      document.querySelector('.resultbox' + theId).appendChild(imageLi) //add li to resultbox 1
+      
+      let newImage = document.createElement('img') //create img tag
+      newImage.src = image; //change source
+      document.querySelector('.resultImage' + theId).appendChild(newImage) //add to li
+  
+      
+  
+  
+  
+      let idLi = document.createElement('li');
+      idLi.innerText = "id: " + theId;
+      idLi.classList.add('idNum');
+      idLi.classList.add('idNum' + theId)
+      document.querySelector('.resultbox' + theId).appendChild(idLi)
+  
+  
+      let nameLi = document.createElement('li');
+      nameLi.innerText = "Name: " + name;
+      nameLi.classList.add("resultName");
+      nameLi.classList.add("resultName" + theId);
+      document.querySelector('.resultbox' + theId).appendChild(nameLi)
+  
+      let genderLi = document.createElement('li');
+      genderLi.innerText = "Gender: " + gender;
+      genderLi.classList.add("resultGender");
+      genderLi.classList.add("resultGender" + theId);
+      document.querySelector('.resultbox' + theId).appendChild(genderLi)
+  
+      let speciesLi = document.createElement('li');
+      speciesLi.innerText = "Species: " + species;
+      speciesLi.classList.add("resultSpecies");
+      speciesLi.classList.add("resultSpecies" + theId);
+      document.querySelector('.resultbox' + theId).appendChild(speciesLi)
+  
+  
+      let typeLi = document.createElement('li');
+      typeLi.innerText = "Type: " + type;
+      typeLi.classList.add("resultType");
+      typeLi.classList.add("resultType" + theId);
+      document.querySelector('.resultbox' + theId).appendChild(typeLi)
+  
+      let originLi = document.createElement('li');
+      originLi.innerText = "Origin: " + origin;
+      originLi.classList.add("resultOrigin");
+      originLi.classList.add("resultOrigin" + theId);
+      document.querySelector('.resultbox' + theId).appendChild(originLi)
+  
+      let locationLi = document.createElement('li');
+      locationLi.innerText = "Location: " + location;
+      locationLi.classList.add("resultLocation");
+      locationLi.classList.add("resultLocation" + theId);
+      document.querySelector('.resultbox' + theId).appendChild(locationLi)
+  
+      let statusLi = document.createElement('li');
+      statusLi.innerText = "Status: " + status;
+      statusLi.classList.add("resultStatus");
+      statusLi.classList.add("resultStatus" + theId);
+      document.querySelector('.resultbox' + theId).appendChild(statusLi)
+     
+     
+      
+      }
 
     
+
+
+
     //inputAll.value = ""
    let urlArr = []
 
